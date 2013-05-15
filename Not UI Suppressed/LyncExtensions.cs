eb17432aa0a8e855c6d6e6830901776d08281461
Jason Young <jason@ytechie.com>
2013-05-15 14:26:04 -0500

    Added logging, reliability fixes, gutted UI

--- Not UI Suppressed/LyncExtensions.cs
+++ Not UI Suppressed/LyncExtensions.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Lync.Model;
 using Microsoft.Lync.Model.Conversation;
@@ -12,6 +10,9 @@ namespace SuperSimpleLyncKiosk
 {
     public static class LyncExtensions
     {
+        private static readonly log4net.ILog Log = log4net.LogManager.GetLogger(
+            System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         public static Task StartAsync(this VideoChannel videoChannel)
         {
             return Task.Factory.FromAsync(videoChannel.BeginStart, videoChannel.EndStart, null);
@@ -19,114 +20,126 @@ namespace SuperSimpleLyncKiosk
 
         public static void AutoAnswerIncomingVideoCalls(this ConversationManager conversationManager)
         {
-            AutoAnswerIncomingVideoCalls(conversationManager, () => true);
-        }
-
-        public static void AutoAnswerIncomingVideoCalls(this ConversationManager conversationManager, Func<bool> predicate)
-        {
-            conversationManager.ConversationAdded += (s, e) => ConversationManager_ConversationAdded(s, e, predicate);
+            conversationManager.ConversationAdded += ConversationManager_ConversationAdded;
+            foreach (var existingConversation in conversationManager.Conversations)
+            {
+                AcceptVideoWhenVideoAdded(existingConversation);
+                GoFullScreenWhenVideoAdded(existingConversation);
+                Log.DebugFormat("Existing conversation with '{0}' found",
+                                string.Join(",", existingConversation.Participants.Select(x => x.Contact.Uri)));
+            }
         }
 
         public static void AnswerVideo(this Conversation conversation)
         {
-            if (conversation.State == ConversationState.Terminated)
+            var converstationState = conversation.State;
+            if (converstationState == ConversationState.Terminated)
             {
                 return;
             }
 
-            var av = (AVModality)conversation.Modalities[ModalityTypes.AudioVideo];
+            var av = (AVModality) conversation.Modalities[ModalityTypes.AudioVideo];
             if (av.CanInvoke(ModalityAction.Connect))
             {
                 av.Accept();
 
                 // Get ready to be connected, then WE can start OUR video
-                av.ModalityStateChanged += AVModality_ModalityStateChanged;
-            }
-        }
-
-        private static void ConversationManager_ConversationAdded(object sender, ConversationManagerEventArgs e, Func<bool> autoAnswerPredicate)
-        {
-            var lync = LyncClient.GetClient();
-            var incomingAV = false;
-            var sb = new StringBuilder();
-            var av = e.Conversation.Modalities[ModalityTypes.AudioVideo];
-            var im = e.Conversation.Modalities[ModalityTypes.InstantMessage];
-
-            // Is this an audio/video invitation?
-            if (av.State == ModalityState.Notified)
-            {
-                if (lync.DeviceManager.ActiveAudioDevice != null)
-                {
-                    sb.Append("Incoming call from ");
-                    incomingAV = true;
-                }
-                else
-                {
-                    av.Reject(ModalityDisconnectReason.NotAcceptableHere);
-                }
-            }
-            if (im.State == ModalityState.Connected)
-            {
-                sb.Append("Incoming IM from ");
-            }
-
-            sb.Append(String.Join(", ", e.Conversation.Participants.Select(i => i.Contact.Uri)));
-            Debug.WriteLine(sb.ToString());
-
-            //eventArgs.Conversation.ParticipantAdded += Conversation_ParticipantAdded;
-            //eventArgs.Conversation.StateChanged += Conversation_ConversationChangedEvent;
-            //eventArgs.Conversation.ActionAvailabilityChanged += Conversation_ActionAvailabilityChanged;
-
-            if (incomingAV && autoAnswerPredicate())
-            {
-                e.Conversation.AnswerVideo();
-            }
-        }
-
-        private static void AVModality_ModalityStateChanged(object sender, ModalityStateChangedEventArgs e)
-        {
-            if (e.NewState == ModalityState.Connected)
-            {
-                // We can't start video until it's connected
-                var vc = ((AVModality)sender).VideoChannel;
-                vc.StateChanged += VideoChannel_StateChanged;
-            }
-        }
-
-        private static void VideoChannel_StateChanged(object sender, ChannelStateChangedEventArgs e)
-        {
-            if (e.NewState != ChannelState.Receive)
-            {
-                return;
-            }
-
-            var vc = (VideoChannel)sender;
-            if (vc.CanInvoke(ChannelAction.Start))
-            {
-                vc.StartAsync().ContinueWith(t =>
-                {
-                    // Go looking around for the IM Window (there had better just be the one we just started)
-                    // and force it to the foreground
-                    IntPtr childHandle = UnsafeNativeMethods.FindWindowEx(IntPtr.Zero, IntPtr.Zero, "IMWindowClass", null);
-                    UnsafeNativeMethods.SetForegroundWindow(childHandle);
-
-                    // Try to get the video to go full screen by pressing F5
-                    WindowsInput.InputSimulator.SimulateKeyPress(WindowsInput.VirtualKeyCode.F5);
-                });
+                //av.ModalityStateChanged += AVModality_ModalityStateChanged;
             }
             else
             {
-                Debug.WriteLine("CanInvoke said NO!");
+                Log.Warn("Unable to start video do to 'CanInvoke' being false");
             }
         }
-    }
 
-    public static class UnsafeNativeMethods
-    {
-        [DllImport("user32.dll")]
-        public static extern IntPtr FindWindowEx(IntPtr parentHandle, IntPtr childAfter, string className, string windowTitle);
+        private static void ConversationManager_ConversationAdded(object sender, ConversationManagerEventArgs e)
+        {
+            try
+            {
+                var avModality = e.Conversation.Modalities[ModalityTypes.AudioVideo];
 
-        [DllImport("user32.dll")]
-        public static extern bool SetForegroundWindow(IntPtr hWnd);
+                //Save the video state so that we avoid wacky things when it changes
+                var avModalityState = avModality.State;
+                Log.DebugFormat("Conversation Added, AV Modality: {0}", avModalityState);
+
+                //Check if the new conversation is a new incoming video request
+                if (avModalityState == ModalityState.Notified)
+                    e.Conversation.AnswerVideo();
+
+                AcceptVideoWhenVideoAdded(e.Conversation);
+                GoFullScreenWhenVideoAdded(e.Conversation);
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Error in ConversationAdded", ex);
+            }
+        }
+
+        private static void AcceptVideoWhenVideoAdded(Conversation conversation)
+        {
+            var avModality = (AVModality) conversation.Modalities[ModalityTypes.AudioVideo];
+            
+            avModality.ModalityStateChanged += (o, args) =>
+                {
+                    try
+                    {
+                        var newState = args.NewState;
+                        Log.DebugFormat("Conversation Modality State Changed to '{0}'", newState);
+
+                        if (newState == ModalityState.Notified)
+                            conversation.AnswerVideo();
+                        if (newState == ModalityState.Connected)
+                            StartOurVideo(avModality);
+                    }
+                    catch (Exception ex)
+                    {
+                        Log.Error("Error handling modality state change", ex);
+                    }
+                };
+        }
+
+        private static void GoFullScreenWhenVideoAdded(Conversation conversation)
+        {
+            var avModality = (AVModality)conversation.Modalities[ModalityTypes.AudioVideo];
+
+            avModality.ModalityStateChanged += (o, args) =>
+            {
+                try
+                {
+                    var newState = args.NewState;
+
+                    if (newState == ModalityState.Notified)
+                    {
+                        var lync = LyncClient.GetAutomation();
+                        var win = lync.GetConversationWindow(conversation);
+                        //win.MoveAndResize(0, 0, (int)SystemParameters.PrimaryScreenWidth, (int)SystemParameters.PrimaryScreenHeight);
+                        win.ShowFullScreen(0);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Log.Error("Error handling modality state change", ex);
+                }
+            };
+        }
+
+        private static void StartOurVideo(AVModality avModality)
+        {
+            var channelStream = avModality.VideoChannel;
+
+            while (!channelStream.CanInvoke(ChannelAction.Start))
+            {
+            }
+
+            channelStream.BeginStart(ar => { }, channelStream);
+            var count = 0;
+            while ((channelStream.State != ChannelState.SendReceive) && (count < 5))
+            {
+                Thread.Sleep(1000);
+
+                channelStream.BeginStart(ar => { }, channelStream);
+                count++;
+            }
+        }
     }
 }
--- Not UI Suppressed/Main.xaml.cs
+++ Not UI Suppressed/Main.xaml.cs
@@ -1,18 +1,34 @@
-﻿using System.Windows;
+﻿using System;
+using System.IO;
+using System.Windows;
 using Microsoft.Lync.Model;
 
 namespace SuperSimpleLyncKiosk
 {
     public partial class Main : Window
     {
+        private static readonly log4net.ILog Log = log4net.LogManager.GetLogger(
+            System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         public Main()
         {
             InitializeComponent();
+            InitLogging();
+
             this.WindowState = System.Windows.WindowState.Maximized;
 
             var lync = LyncClient.GetClient();
 
-            lync.ConversationManager.AutoAnswerIncomingVideoCalls(() => Properties.Settings.Default.autoAnswer);
+            lync.ConversationManager.AutoAnswerIncomingVideoCalls();
+
+            Log.Debug("Kiosk Started");
+        }
+
+        public void InitLogging()
+        {
+            var logConfig = new FileInfo("Logging.config");
+            log4net.Config.XmlConfigurator.ConfigureAndWatch(logConfig);
+            Log.Info("Lync Kiosk Starting");
         }
     }
 }
